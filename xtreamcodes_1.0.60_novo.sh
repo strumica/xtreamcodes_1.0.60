5813440f5717e206118d4a4aa0f2bb1c2b5bc193
marconimp <marconimp@yahoo.com.br>
2017-05-12 12:24:48 -0300

    Update xtreamcodes_1.0.60_novo.sh

--- xtreamcodes_1.0.60_novo.sh
+++ xtreamcodes_1.0.60_novo.sh
@@ -362,7 +362,21 @@ echo -e "${jeshile} └───────────────────
 echo " "
 #mkdir /var/www/html
 #cd /var/www/html #DESTINACIONI KRYESOR ADMIN - TEMPLATES ETC
-cd /var/www/html && wget https://www.dropbox.com/s/2is9fp9c2x7q80v/Xtream_Codes_v1.0.60_Nulled.zip && unzip Xtream_Codes_v1.0.60_Nulled.zip && cp /var/www/html/downloads/iptv_panel_pro.zip /tmp && chmod a+x /tmp/iptv_panel_pro.zip && cp /var/www/html/downloads/install_iptv_pro.php /root/ && cd /root && chmod a+x /root/install_iptv_pro.php && php install_iptv_pro.php
+cd /home/ubuntu
+sudo su
+apt-get install unrar
+wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part01.rar
+wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part02.rar
+wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part03.rar
+wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part04.rar
+wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part05.rar
+wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part06.rar
+wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part07.rar
+wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part08.rar
+wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part09.rar
+sudo unrar x go.part01.rar
+cp -R /home/ubuntu/go/Xtream_Codes_v1.0.60_Nulled.zip  /var/www/html/
+cd /var/www/html && unzip Xtream_Codes_v1.0.60_Nulled.zip && cp /var/www/html/downloads/iptv_panel_pro.zip /tmp && chmod a+x /tmp/iptv_panel_pro.zip && cp /var/www/html/downloads/install_iptv_pro.php /root/ && cd /root && chmod a+x /root/install_iptv_pro.php && php install_iptv_pro.php
 chmod 775 /var/www/html/Xtream_Codes_v1.0.60_Nulled.zip
 rm /var/www/html/Xtream_Codes_v1.0.60_Nulled.zip
 cp /var/www/html/downloads/iptv_panel_pro.zip /tmp
