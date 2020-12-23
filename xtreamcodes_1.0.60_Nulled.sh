cbffda6ea74387d5c5ae0c854d4b1ad26defece0
Roxy Squires <squires1993@gmail.com>
2020-12-23 05:23:14 +0000

    Updated to work with php5.6

--- xtreamcodes_1.0.60_Nulled.sh
+++ xtreamcodes_1.0.60_Nulled.sh
@@ -20,16 +20,16 @@ echo -e "${jeshile} │   Installing Web Server   │ \e[0m"
 echo -e "${jeshile} └───────────────────────────┘ \e[0m"
 echo " "
 apt-get update && apt-get upgrade -y && apt-get install htop -y
-apt-get install lsb-release nscd curl php5 php5-mysql php5-cli php5-curl unzip curl libcurl3 libcurl3-dev php5-curl -y
+apt-get install lsb-release nscd curl php5.6 php5.6-mysql php5.6-cli php5.6-curl unzip curl libcurl3 libcurl3-dev php5.6-curl -y
 sudo apt-get update && sudo apt-get install vlc vlc-plugin-* -y && sudo apt-get install vlc browser-plugin-vlc -y
 sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl && chmod a+rx /usr/local/bin/youtube-dl
 sudo updatedb
 sudo apt-get update
-sudo apt-get install php5-dev php5-gd -y
-apt-get install php5-geoip php5-fpm mcrypt php5-mcrypt php5enmod mcrypt -y
-apt-get install apache2 apache2-mpm-prefork apache2-utils libapache2-mod-php5 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libnet-daemon-perl libplrpc-perl libpq5 mysql-client-5.5 mysql-common mysql-server mysql-server-5.5 php5-common php5-mysql -y
+sudo apt-get install php5.6-dev php5.6-gd -y
+apt-get install php5.6-geoip php5.6-fpm mcrypt php5.6-mcrypt php5.6enmod mcrypt -y
+apt-get install apache2 apache2-mpm-prefork apache2-utils libapache2-mod-php5.6 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libnet-daemon-perl libplrpc-perl libpq5 mysql-client-5.5 mysql-common mysql-server mysql-server-5.5 php5.6-common php5.6-mysql -y
 apt-get install phpmyadmin -y
-sudo php5enmod mcrypt
+sudo php5.6enmod mcrypt
 service apache2 reload && service apache2 restart
 echo " "
 echo -e "${jeshile} ┌────────────────────────────────┐ \e[0m"
